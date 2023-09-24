4a74e55dca5878952b85197248465f2178aa98eb
Yasar Avci <yasaravc5@gmail.com>
2023-09-24 17:37:41 +0300

    commit
    proje sildim, yenisini ekledim

--- deneme/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace deneme.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ gitHubDeneme/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gitHubDeneme.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
